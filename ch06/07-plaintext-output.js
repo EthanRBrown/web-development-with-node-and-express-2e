3aa6aace2bb8d57e69891f43213efe192190fbf0
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2024-02-24 17:23:52 +0700

    Correct spelling test -> text

--- ch06/07-plaintext-output.js
+++ ch06/07-plaintext-output.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/text', (req, res) => {
   res.type('text/plain')
-  res.send('this is a test')
+  res.send('this is a text')
 })
 
 app.get('*', (req, res) => res.send('Check out the "<a href="/text">plain text</a>" page!'))
